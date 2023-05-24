2ea8566b414f91092cea8e5f28f3b3f21fecd717
tarkik <prokop.romanski@gmail.com>
2023-05-24 22:03:25 +0300

    feat: moved to bootstrap 5

--- index.php
+++ index.php
@@ -1,49 +1,44 @@
 <!DOCTYPE html>
+
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Robot.txt analyzing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <style>
-        body {
-            padding: 28px;
-        }
-
-        #result {
-            margin-top: 32px;
-        }
-
         .ok {
-            background: #2ecc71;
+        background: #2ecc71;
         }
 
         .err {
-            background: #e74c3c;
+        background: #e74c3c;
         }
     </style>
 </head>
 <body>
-    <div class="form-group row">
-        <label for="enterSite" class="col-sm-2 form-control-label">Адресс сайта: </label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="enterSite" placeholder="http://seasonvar.ru">
+    <div class="container py-3">
+        <label for="search-url" class="form-label">Enter URL:</label>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="https://www.your-url.com" id="search-url">
         </div>
-    </div>
-    <button class="btn btn-primary-outline btn-lg btn-block" type="button" id="exploreSite">Проверить</button>
 
-    <div id="result">
+        <div class="d-grid gap-2">
+            <button class="btn btn-outline-primary btn-lg" type="button" id="exploreSite">Run analyzing!</button>
+        </div>
 
+        <div id="result"></div>
     </div>
 
     <script>
         $('#exploreSite').click(function () {
-            $('#result').html("Загрузка...");
+            $('#result').html("Loading...");
             $.get(
                 'model.php',
                 {
-                    url: $('#enterSite').val()
+                    url: $('#search-url').val()
                 },
                 function(data){
                     $('#result').html(data);
